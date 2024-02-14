0dc06809333d41e83aa16a588a1c724f556d1231
Arnaud Turcotte <arnaudt09@gmail.com>
2024-02-14 17:38:04 -0500

    PROGRESS: Début de la Task OnConnectedAsnc

--- Super Cartes Infinies/Hubs/MatchHub.cs
+++ Super Cartes Infinies/Hubs/MatchHub.cs
@@ -1,12 +1,44 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Super_Cartes_Infinies.Data;
+using Super_Cartes_Infinies.Models;
+using Super_Cartes_Infinies.Services;
 
 namespace Super_Cartes_Infinies.Hubs;
 
-[Authorize]
+// [Authorize]
+
+
+
 public class MatchHub : Hub
 {
-    public MatchHub()
+    ApplicationDbContext _context;
+    MatchesService _matchesService;
+    WaitingUserService _waitingUserService;
+
+    public IdentityUser CurentUser
     {
+        get
+        {
+            // On récupère le userid à partir du Cookie qui devrait être envoyé automatiquement
+            string userid = Context.UserIdentifier!;
+            return _context.Users.Single(u => u.Id == userid);
+        }
+    }
+
+    public MatchHub(ApplicationDbContext context, MatchesService matchesService, WaitingUserService waitingUserService)
+    {
+        _context = context;
+        _matchesService = matchesService;
+        _waitingUserService = waitingUserService;
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        await base.OnConnectedAsync();
+        UserHandler.ConnectedIds.Add(Context.ConnectionId);
+
     }
 }
