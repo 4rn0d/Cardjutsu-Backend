00d70d90618e67c13785fb6bc4b560a36ac41db3
Arnaud Turcotte <arnaudt09@gmail.com>
2024-02-14 14:56:57 -0500

    FCT : Ajout de la logique pour get tout les cartes de départ

--- Super Cartes Infinies/Combat/StartMatchEvent.cs
+++ Super Cartes Infinies/Combat/StartMatchEvent.cs
@@ -1,14 +1,20 @@
 ﻿using Super_Cartes_Infinies.Models;
+using Super_Cartes_Infinies.Services;
 
 namespace Super_Cartes_Infinies.Combat
 {
     public class StartMatchEvent : MatchEvent
     {
+        public MatchConfigurationService _matchConfigurationService;
+
         public StartMatchEvent(Match match, MatchPlayerData currentPlayerData, MatchPlayerData opposingPlayerData, int nbStartingCards, int nbManaPerTurn)
         {
             Events = new List<MatchEvent> { };
 
-            Events.Add(new PlayerStartTurnEvent(currentPlayerData));
+            for (int i = 0; i < _matchConfigurationService.GetNbStartingCards(); i++)
+            {
+                Events.Add(new PlayerStartTurnEvent(currentPlayerData));
+            }
         }
     }
 }
