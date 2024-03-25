2c9811f5d1da474566a5fa1dacf34310728c8573
Arnaud Turcotte <arnaudt09@gmail.com>
2024-03-25 14:03:19 -0400

    FCT : ajout du virtual de owned cards

--- Super Cartes Infinies/Models/Deck.cs
+++ Super Cartes Infinies/Models/Deck.cs
@@ -7,6 +7,6 @@ namespace Super_Cartes_Infinies.Models
         public int DeckId { get; set; }
         public string DeckName { get; set; }
         public bool IsCurrentDeck { get; set; }
-        public List<OwnedCard> OwnedCards { get; set; }
+        public virtual List<OwnedCard> OwnedCards { get; set; }
     }
 }
