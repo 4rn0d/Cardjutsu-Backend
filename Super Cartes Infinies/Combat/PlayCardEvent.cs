71ba8ff30f2b4c3ef0dd0002d9adb280154bbeb7
Arnaud Turcotte <arnaudt09@gmail.com>
2024-04-17 09:30:29 -0400

    FCT : Modification du PlayCardEvent pour qu'il fonctionne

--- Super Cartes Infinies/Combat/PlayCardEvent.cs
+++ Super Cartes Infinies/Combat/PlayCardEvent.cs
@@ -7,27 +7,28 @@ namespace Super_Cartes_Infinies.Combat
     {
 
         // TODO: Ajouter tout ce qui manque
-        public PlayCardEvent(MatchPlayerData currentPlayerData, MatchPlayerData opposingPlayerData, int playableCardId)
+
+        public int PlayableCardId { get; set; }
+        public int PlayerId { get; set; }
+        public int Mana {get; set;}
+         public PlayCardEvent(MatchPlayerData currentPlayerData, MatchPlayerData opposingPlayerData, int playableCardId)
         {
             PlayableCard playedcard = currentPlayerData.Hand.Where(c => c.Id == playableCardId).FirstOrDefault();
 
-            if (currentPlayerData.Mana - playedcard.Card.Cost <0)
+            if (currentPlayerData.Mana - playedcard.Card.Cost >= 0)
+            {
+                PlayerId = currentPlayerData.PlayerId;
+                PlayableCardId = playedcard.Id;
+                Mana = currentPlayerData.Mana - playedcard.Card.Cost;
+                currentPlayerData.BattleField.Add(playedcard);
+                currentPlayerData.Hand.Remove(playedcard);
+                currentPlayerData.Mana -= playedcard.Card.Cost;
+            }
+            else
             {
                 throw new Exception("Le joueur n'a pas assez de mana pour jouer la carte");
             }
-            currentPlayerData.BattleField.Add(playedcard);
-            currentPlayerData.Mana -= playedcard.Card.Cost;
 
-
-            //if(playedcard.Health == 0)
-            //{
-            //    currentPlayerData.BattleField.Remove(playedcard);
-            //    currentPlayerData.Graveyard.Add(playedcard);
-            //}
-
-
-            
-            
         }
     }
 }
