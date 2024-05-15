8c4a6bac8f1b2477e4afb5819a99e83f77e3d001
Sarah <2050189@cegepmontpetit.ca>
2024-05-14 20:26:27 -0400

    FCT Menus Leaderboard et joueurs ELO similiares

--- Super Cartes Infinies/Controllers/ScoreboardController.cs
+++ Super Cartes Infinies/Controllers/ScoreboardController.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Super_Cartes_Infinies.Data;
 using Super_Cartes_Infinies.Models;
+using Super_Cartes_Infinies.ViewModels;
 
 namespace Super_Cartes_Infinies.Controllers
 {
+
+    [Route("api/[controller]/[action]")]
+    [ApiController]
     public class ScoreboardController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -20,27 +25,70 @@ namespace Super_Cartes_Infinies.Controllers
         }
 
         // GET: best 8 Players
-        public async Task<IActionResult> GetBestPlayersScore()
+        [HttpGet]
+        public async Task<ActionResult<List<Player>>> GetBestPlayersScore()
         {
             var applicationDbContext = _context.Players.OrderByDescending(p => p.EloScore).Take(8);
-            return View(await applicationDbContext.ToListAsync());
+            return await applicationDbContext.ToListAsync();
         }
 
         // GET: similar 8 Players
-        public async Task<IActionResult> GetSimilarPlayersScore(int id)
+        [HttpGet("{username}")]
+        public async Task<ActionResult<List<PositionPlayerVM>>> GetSimilarPlayersScore(string username)
         {
 
-            Player currentP = _context.Players.Single(p => p.Id == id);
+            Player currentP = _context.Players.Single(p => p.Name == username);
 
             //TODO
 
-            var applicationDbContext = _context.Players.OrderByDescending(p => p.EloScore).Take(8);
+            List<PositionPlayerVM> list = new List<PositionPlayerVM>();
 
-            if(applicationDbContext.Contains(currentP)) 
+            var leaderboard = await _context.Players.OrderByDescending(p => p.EloScore).ToListAsync();
+
+            var best5Players = await _context.Players.OrderByDescending(p => p.EloScore).Take(5).ToListAsync();
+            var worst5Players = await _context.Players.OrderBy(p => p.EloScore).Take(5).ToListAsync();
+
+
+            List<PositionPlayerVM> customlist = new List<PositionPlayerVM>();
+
+            foreach (var player in leaderboard)
             {
-                return View(await applicationDbContext.ToListAsync());
+                list.Add(new PositionPlayerVM
+                {
+                    Position = (leaderboard.IndexOf(player)+1),
+                    Username = player.Name,
+                    ELO = player.EloScore
+                });
             }
-            return View(await applicationDbContext.ToListAsync());
+
+
+
+            if (best5Players.Contains(currentP))
+            {
+                customlist = list.Take(8).ToList();
+                return customlist;
+            }
+
+            if(worst5Players.Contains(currentP))
+            {
+
+                customlist = list.TakeLast(8).ToList();
+                return customlist;
+            }
+
+            int indexOfCurrent = (leaderboard.IndexOf(currentP)+1);
+
+
+            for(int i = (indexOfCurrent-4); i < (indexOfCurrent+4); i++)
+            {
+                customlist.Add(list.ElementAt(i));
+            }
+
+
+
+
+            return customlist;
+
         }
 
         
---
+++ Super Cartes Infinies/ViewModels/PositionPlayerVM.cs
@@ -0,0 +1,11 @@
+﻿namespace Super_Cartes_Infinies.ViewModels
+{
+    public class PositionPlayerVM
+    {
+        public int Position { get; set; }
+
+        public string Username { get; set; }
+
+        public int ELO { get; set; }
+    }
+}
